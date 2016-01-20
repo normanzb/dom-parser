8573cb9cf818db6a40f68bfccdebcfbde05b63a0
Norman Xu <im@norm.im>
2016-01-20 15:34:48 +0000

    first commit

---
+++ src/DOMParser.js
@@ -0,0 +1,39 @@
+define(function() {
+    'use strict';
+
+    var DOMParser = this.DOMParser;
+
+    if (DOMParser == null) {
+        DOMParser = function(){};
+    }
+
+    var DOMParser_proto = DOMParser.prototype, 
+        real_parseFromString = DOMParser_proto.parseFromString;
+
+    // Firefox/Opera/IE throw errors on unsupported types
+    try {
+        // WebKit returns null on unsupported types
+        if ((new DOMParser()).parseFromString('', 'text/html')) {
+            // text/html parsing is natively supported
+            return DOMParser;
+        }
+    } catch (ex) {}
+
+    DOMParser_proto.parseFromString = function(markup, type) {
+        if (/^\s*text\/html\s*(?:;|$)/i.test(type)) {
+            var
+              doc = document.implementation.createHTMLDocument('')
+            ;
+                if (markup.toLowerCase().indexOf('<!doctype') > -1) {
+                    doc.documentElement.innerHTML = markup;
+                }
+                else {
+                    doc.body.innerHTML = markup;
+                }
+            return doc;
+        } else {
+            return real_parseFromString.apply(this, arguments);
+        }
+    };
+    return DOMParser;
+});
